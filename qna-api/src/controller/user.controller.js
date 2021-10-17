ebe9e03c3b23c142ba4b32d548d8c9038f53a10a
Clyde Tedrick <clydet@users.noreply.github.com>
2021-10-17 16:09:28 -0400

    Main --> develop (#5)
    * MIT License
    
    * issue 3: initial project (#4)
    
    * 3: generated api
    
    * 3: additional test coverage
    
    * 3: refactor away from emoji
    
    * 3: first draft api ci
    
    * actions type-o?
    
    * 3: qualify api working directory
    
    * 3: don't support node 10

---
+++ qna-api/src/controller/user.controller.js
@@ -0,0 +1,1 @@
+module.exports.getAllUsers = () => ['Tom', 'Dick', 'Harry'];
---
+++ qna-api/test/api.test.js
@@ -0,0 +1,51 @@
+const request = require('supertest');
+
+const app = require('../src/api');
+
+describe('GET /api/v1', () => {
+  const isValidErrorMessage = (res) => res.body.message === '🔍 - Not Found - /api/v1/';
+
+  it('responds with a 404', (done) => {
+    request(app)
+      .get('/api/v1')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .expect(isValidErrorMessage)
+      .end(done);
+  });
+});
+
+describe('GET /api/v1/users', () => {
+  it('responds with a json message', (done) => {
+    request(app)
+      .get('/api/v1/users')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, ['Tom', 'Dick', 'Harry'], done);
+  });
+});
+
+describe('GET missing api endpoint', () => {
+  let nodeEnv;
+  beforeEach(() => {
+    nodeEnv = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'production';
+  });
+
+  afterEach(() => {
+    process.env.NODE_ENV = nodeEnv;
+  });
+
+  it('responds with a json message', (done) => {
+    const isValidErrorMessage = (res) => res.body.message === '🔍 - Not Found - /api/v1/nope';
+
+    request(app)
+      .get('/api/v1/nope')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .expect(isValidErrorMessage)
+      .end(done);
+  });
+});
